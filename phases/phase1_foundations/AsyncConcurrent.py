09cafb708ab1f860449b6bb56962ed520bd2e1ae
sanikalj <nikalje.503@gmail.com>
2026-06-13 19:49:56 +0100

    Add project folder structure

---
+++ phases/phase1_foundations/AsyncConcurrent.py
@@ -0,0 +1,14 @@
+import asyncio
+
+async def task(name,delay):
+    await asyncio.sleep(delay)
+    print(f"{name} finished")
+
+async def main():
+    await asyncio.gather(
+        task("Task A",2),
+        task("Task B", 1),
+        task("Task C", 3)
+    )
+
+asyncio.run(main())
